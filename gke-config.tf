797a613597c7f367731e7c0d1dbaa982ae0225e3
amce <amce@gftdevgcp.com>
2021-03-23 16:34:09 +0000

    fixed issue

--- gke-config.tf
+++ gke-config.tf
@@ -3,7 +3,7 @@ resource "null_resource" "kubectl" {
   //    always = timestamp()
   //  }
   provisioner "local-exec" {
-    command     = "gcloud compute ssh ${var.forward_proxy_name} --project ${var.project_id} --zone ${var.forward_proxy_zone} -- -L 3128:localhost:3128 -N -q -f"
+    command     = "gcloud compute ssh ${var.forward_proxy_name} --project ${var.project_id} --zone ${var.zone} -- -L 3128:localhost:3128 -N -q -f"
     interpreter = ["bash", "-c"]
   }
 }
