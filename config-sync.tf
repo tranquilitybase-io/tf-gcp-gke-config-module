104d96d1a371c7e9e748c1bebc8a5ab860c33ebd
zi01farm <zaid.iqbal@gft.com>
2021-04-13 10:49:47 +0100

    Updates config-sync

--- config-sync.tf
+++ config-sync.tf
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+###
+# Deploy and configure config sync
+###
+
 resource "local_file" "config-management" {
   filename = "./config-management.yaml"
   content = yamlencode({
