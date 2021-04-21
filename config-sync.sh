e9a42c6441ad600b27c2cc54c1b9353f051977ac
amce <adamgft@gft.com>
2021-04-21 10:24:47 +0100

    test

--- config-sync.sh
+++ config-sync.sh
@@ -2,7 +2,11 @@ gcloud container clusters get-credentials $1 --project $2 --zone $3
 gcloud compute ssh $4 --project $2 --zone $5 --tunnel-through-iap -- -L 3128:localhost:3128 -N -q -f
 sleep 10
 echo "###"
-echo $6
+for i in "${!$6[@]}"
+do
+  echo "key  : $i"
+  echo "value: ${array[$i]}"
+done
 echo "###"
 pid=$(pidof ssh)
 HTTPS_PROXY=localhost:3128 gsutil cp gs://config-management-release/released/latest/config-sync-operator.yaml ./config-sync-operator.yaml
@@ -10,10 +14,4 @@ HTTPS_PROXY=localhost:3128 kubectl apply -f config-sync-operator.yaml
 HTTPS_PROXY=localhost:3128 cat config-management.yaml
 HTTPS_PROXY=localhost:3128 kubectl apply -f config-management.yaml
 HTTPS_PROXY=localhost:3128 kubectl get all --all-namespaces
-echo "###"
-echo $6
-echo "###"
 kill $pid
-echo "###"
-echo $6
-echo "###"
