b6ad352ae689353571fd4241d5d8039f5d1e40f6
amce <adamgft@gft.com>
2021-04-23 10:24:52 +0100

    remove overwrite + put in project

--- config-sync.sh
+++ config-sync.sh
@@ -7,8 +7,8 @@ HTTPS_PROXY=localhost:3128 kubectl apply -f config-sync-operator.yaml
 HTTPS_PROXY=localhost:3128 cat config-management.yaml
 HTTPS_PROXY=localhost:3128 kubectl apply -f config-management.yaml
 HTTPS_PROXY=localhost:3128 kubectl get all --all-namespaces
-IFS=$'\n' read -r -d '' -a serviceaccounts < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=foo=bar -o yaml | grep "  name: " | cut -f2 -d":" && printf '\0' )
-IFS=$'\n' read -r -d '' -a namespaces < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=foo=bar -o yaml | grep "  namespace: " | cut -f2 -d":" && printf '\0' )
+IFS=$'\n' read -r -d '' -a serviceaccounts < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=wi=true -o yaml | grep "  name: " | cut -f2 -d":" && printf '\0' )
+IFS=$'\n' read -r -d '' -a namespaces < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=wi=true -o yaml | grep "  namespace: " | cut -f2 -d":" && printf '\0' )
 for KEY in "${!namespaces[@]}"; do
    HTTPS_PROXY=localhost:3128 kubectl annotate serviceaccount --namespace "${namespaces[$KEY]:1}" "${serviceaccounts[$KEY]:1}" iam.gke.io/gcp-service-account="${serviceaccounts[$KEY]:1}"@$2.iam.gserviceaccount.com
 done
