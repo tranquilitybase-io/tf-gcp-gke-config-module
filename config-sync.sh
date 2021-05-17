fa378fad2222d1616fe52bc41bebf2e2760b25a0
adamgft <66415638+adamgft@users.noreply.github.com>
2021-05-17 17:48:52 +0100

    test

--- config-sync.sh
+++ config-sync.sh
@@ -11,9 +11,6 @@ sleep 300
 IFS=$'\n' read -r -d '' -a serviceaccounts < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=wi=true -o yaml | grep "  name: " | cut -f2 -d":" && printf '\0' )
 IFS=$'\n' read -r -d '' -a namespaces < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=wi=true -o yaml | grep "  namespace: " | cut -f2 -d":" && printf '\0' )
 for KEY in "${!namespaces[@]}"; do
-   echo "--${namespaces[$KEY]:1}--${serviceaccounts[$KEY]:1}"
-   HTTPS_PROXY=localhost:3128 kubectl annotate serviceaccount --namespace "${namespaces[$KEY]:1}" "${serviceaccounts[$KEY]:1}" iam.gke.io/gcp-service-account="${serviceaccounts[$KEY]:1}"@$2.iam.gserviceaccount.com
    HTTPS_PROXY=localhost:3128 kubectl annotate serviceaccount --namespace "${namespaces[$KEY]:1}" "${serviceaccounts[$KEY]:1}" iam.gke.io/gcp-service-account="${serviceaccounts[$KEY]:1}"@$2.iam.gserviceaccount.com --overwrite
-   echo "--${namespaces[$KEY]:1}--${serviceaccounts[$KEY]:1}"
 done
 kill $pid
