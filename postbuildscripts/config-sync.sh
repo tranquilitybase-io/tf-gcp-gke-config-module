e6bad9b10d509e3edce38f9798a5eb3b00cf8031
hisasi <hisasi@gmail.com>
2021-05-26 13:29:55 +0100

    Update config-sync.sh

--- postbuildscripts/config-sync.sh
+++ postbuildscripts/config-sync.sh
@@ -23,6 +23,9 @@ then
 else
 	echo "Installing Istion verison  $ISTIO_VERSION"
 	echo "current directory: $PWD"
+	DIRECTORY=`dirname $0`
+	echo $DIRECTORY
+	cd $DIRECTORY
 	chmod +x ./istio.sh
         ./istio.sh $ISTIO_VERSION
 fi
