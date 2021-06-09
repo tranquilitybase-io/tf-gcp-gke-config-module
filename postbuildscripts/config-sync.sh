8f07656c7493f163cb0796b3b6b8bc8ed78d7816
e-amce <adam.crow@gft.com>
2021-06-09 12:42:03 +0100

    test

--- postbuildscripts/config-sync.sh
+++ postbuildscripts/config-sync.sh
@@ -1,5 +1,6 @@
-gcloud container clusters get-credentials $1 --project $2 --zone $3
-gcloud compute ssh $4 --project $2 --zone $5 --tunnel-through-iap -- -L 3128:localhost:3128 -N -q -f
+gcloud config list
+#gcloud container clusters get-credentials $1 --project $2 --zone $3
+#gcloud compute ssh $4 --project $2 --zone $5 --tunnel-through-iap -- -L 3128:localhost:3128 -N -q -f
 #sleep 10
 #pid=$(pidof ssh)
 #HTTPS_PROXY=localhost:3128 gsutil cp gs://config-management-release/released/latest/config-sync-operator.yaml ./config-sync-operator.yaml
