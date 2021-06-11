e4c77c0fdfc13986cff2a7fa3e1e75e8eb5d47af
e-amce <adam.crow@gft.com>
2021-06-11 10:51:55 +0100

    fmt

--- config-sync.tf
+++ config-sync.tf
@@ -21,18 +21,18 @@ resource "local_file" "config-sync-management-yaml" {
   content = yamlencode({
     "apiVersion" : "configmanagement.gke.io/v1",
     "kind" : "ConfigManagement",
-    "metadata" : {"name" = "config-management"},
-    "spec" : {"clusterName" = var.cluster_name,"git" : {"syncRepo" = var.sync_url,"syncBranch" = var.sync_branch,"secretType" = var.secret_type,"policyDir" = var.root_manifest_folder_name}}
-  }
+    "metadata" : { "name" = "config-management" },
+    "spec" : { "clusterName" = var.cluster_name, "git" : { "syncRepo" = var.sync_url, "syncBranch" = var.sync_branch, "secretType" = var.secret_type, "policyDir" = var.root_manifest_folder_name } }
+    }
   )
 }
 
 resource "null_resource" "gke-config" {
-    triggers = {
-      always = timestamp()
-    }
+  triggers = {
+    always = timestamp()
+  }
   provisioner "local-exec" {
-    command     = "./postbuildscripts/config-sync.sh ${var.cluster_name} ${var.project_id} ${var.cluster_region} ${var.forward_proxy_name} ${var.forward_proxy_zone} ${var.istio_version}"
+    command = "./postbuildscripts/config-sync.sh ${var.cluster_name} ${var.project_id} ${var.cluster_region} ${var.forward_proxy_name} ${var.forward_proxy_zone} ${var.istio_version}"
 
     interpreter = ["bash", "-c"]
   }
