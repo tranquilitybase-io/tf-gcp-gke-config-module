9441d0d85d12b1d4b14f977fff70c7a31f15883f
e-amce <adam.crow@gft.com>
2021-06-16 14:10:33 +0100

    test

--- postbuildscripts/cft-org.sh
+++ postbuildscripts/cft-org.sh
@@ -1,10 +1,7 @@
 git clone https://github.com/terraform-google-modules/terraform-example-foundation.git
 cp ./org.tfvars ./terraform-example-foundation/1-org/envs/shared
 rm terraform.example.tfvars
-echo ---
-echo $TG_BUCKET
-echo ---
-#for i in `find -name 'backend.tf'`; do sed -i 's/UPDATE_ME/$1/' $i; done
+for i in `find -name 'backend.tf'`; do sed -i 's/UPDATE_ME/$TG_BUCKET/' $i; done
 cd ./terraform-example-foundation/1-org
 cp ../build/tf-wrapper.sh .
 chmod 755 ./tf-wrapper.sh
