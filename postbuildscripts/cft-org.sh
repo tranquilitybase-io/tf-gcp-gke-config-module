eb3e87d2ea72439979ecf38fb1d677f80cb52d04
e-amce <adam.crow@gft.com>
2021-06-16 12:32:08 +0100

    test

--- postbuildscripts/cft-org.sh
+++ postbuildscripts/cft-org.sh
@@ -1,12 +1,11 @@
 git clone https://github.com/terraform-google-modules/terraform-example-foundation.git
-ls -la
-echo dir1
 cp ./org.tfvars ./terraform-example-foundation/1-org/envs/shared
+rm terraform.example.tfvars
 cd ./terraform-example-foundation/1-org
 cp ../build/tf-wrapper.sh .
 chmod 755 ./tf-wrapper.sh
 cd ./envs/shared/
-pwd
 ls -la
-echo dir2
 terraform --version
+terraform init
+terraform plan
