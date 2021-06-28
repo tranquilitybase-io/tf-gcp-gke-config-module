98eb9c6111b6680190d244d10f25df41a8937611
adamgft <66415638+adamgft@users.noreply.github.com>
2021-06-28 17:39:02 +0100

    Update cft-org.sh

--- postbuildscripts/cft-org.sh
+++ postbuildscripts/cft-org.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/adamgft/terraform-example-foundation.git
+git clone https://github.com/terraform-google-modules/terraform-example-foundation.git
 cp ./org.auto.tfvars ./terraform-example-foundation/1-org/envs/shared
 for i in `find -name 'backend.tf'`; do sed -i 's/UPDATE_ME/'$TG_BUCKET'/' $i; done
 cd ./terraform-example-foundation/1-org
