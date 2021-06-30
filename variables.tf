2050f182fc4d04ca04831a89aa907b4159e8f49a
adamgft <66415638+adamgft@users.noreply.github.com>
2021-06-30 16:09:50 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -101,7 +101,7 @@ variable "workload_identity_service_account" {
 variable "domains_to_allow" {
   description = ""
   type = list(string)
-  default = ["tranquilitybase-demo.io"]
+  default = ["TRANQUILITYBASE-DEMO.IO"]
 }
 
 variable "billing_data_users" {
