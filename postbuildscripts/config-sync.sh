e7cbb9c3c153e5ce0d48e61663c41ae0dce43fec
adamgft <66415638+adamgft@users.noreply.github.com>
2021-07-01 13:52:05 +0100

    Update config-sync.sh

--- postbuildscripts/config-sync.sh
+++ postbuildscripts/config-sync.sh
@@ -5,30 +5,30 @@ pid=$(pidof ssh)
 HTTPS_PROXY=localhost:3128 gsutil cp gs://config-management-release/released/latest/config-sync-operator.yaml ./config-sync-operator.yaml
 HTTPS_PROXY=localhost:3128 kubectl apply -f config-sync-operator.yaml
 HTTPS_PROXY=localhost:3128 cat config-management.yaml
-# HTTPS_PROXY=localhost:3128 kubectl apply -f config-management.yaml
-# HTTPS_PROXY=localhost:3128 kubectl get all --all-namespaces
-# #sleep 300
-# IFS=$'\n' read -r -d '' -a serviceaccounts < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=wi=true -o yaml | grep "  name: " | cut -f2 -d":" && printf '\0' )
-# IFS=$'\n' read -r -d '' -a namespaces < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=wi=true -o yaml | grep "  namespace: " | cut -f2 -d":" && printf '\0' )
-# for KEY in "${!namespaces[@]}"; do
-#    HTTPS_PROXY=localhost:3128 kubectl annotate serviceaccount --namespace "${namespaces[$KEY]:1}" "${serviceaccounts[$KEY]:1}" iam.gke.io/gcp-service-account="${serviceaccounts[$KEY]:1}"@$2.iam.gserviceaccount.com --overwrite
-# done
+HTTPS_PROXY=localhost:3128 kubectl apply -f config-management.yaml
+HTTPS_PROXY=localhost:3128 kubectl get all --all-namespaces
+#sleep 300
+IFS=$'\n' read -r -d '' -a serviceaccounts < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=wi=true -o yaml | grep "  name: " | cut -f2 -d":" && printf '\0' )
+IFS=$'\n' read -r -d '' -a namespaces < <( HTTPS_PROXY=localhost:3128 kubectl get serviceaccount --all-namespaces --selector=wi=true -o yaml | grep "  namespace: " | cut -f2 -d":" && printf '\0' )
+for KEY in "${!namespaces[@]}"; do
+   HTTPS_PROXY=localhost:3128 kubectl annotate serviceaccount --namespace "${namespaces[$KEY]:1}" "${serviceaccounts[$KEY]:1}" iam.gke.io/gcp-service-account="${serviceaccounts[$KEY]:1}"@$2.iam.gserviceaccount.com --overwrite
+done
 
 
-# ISTIO_VERSION="$6"
-# if [ -z "$ISTIO_VERSION" ] 
-# then
-# 	echo "Skipped Installing Istio - No Version found"
+ISTIO_VERSION="$6"
+if [ -z "$ISTIO_VERSION" ] 
+then
+	echo "Skipped Installing Istio - No Version found"
  
-# else
-# 	echo "Installing Istion verison  $ISTIO_VERSION"
-# 	DIR=`dirname $0`
-# 	echo "Scirpt Location: $DIRECTORY"
-# 	cd $DIR
-# 	chmod +x ./istio.sh
-#         ./istio.sh $ISTIO_VERSION
-# fi
+else
+	echo "Installing Istion verison  $ISTIO_VERSION"
+	DIR=`dirname $0`
+	echo "Scirpt Location: $DIRECTORY"
+	cd $DIR
+	chmod +x ./istio.sh
+        ./istio.sh $ISTIO_VERSION
+fi
 
-#./cft-org.sh
+./cft-org.sh
 
 kill $pid
