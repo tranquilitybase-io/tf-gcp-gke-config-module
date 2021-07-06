1617efe9bcea9c3e4311032cbb96eb45bc4b83af
Mohiddin Shaik <mshaik@lseg.com>
2021-07-06 17:28:06 +0100

    enabled debug + exit in script

--- postbuildscripts/config-sync.sh
+++ postbuildscripts/config-sync.sh
@@ -1,6 +1,8 @@
+set -ex
+
 gcloud container clusters get-credentials $1 --project $2 --zone $3
 gcloud compute ssh $4 --project $2 --zone $5 --tunnel-through-iap -- -L 3128:localhost:3128 -N -q -f
-sleep 100
+sleep 200
 pid=$(pidof ssh)
 HTTPS_PROXY=localhost:3128 gsutil cp gs://config-management-release/released/latest/config-sync-operator.yaml ./config-sync-operator.yaml
 HTTPS_PROXY=localhost:3128 kubectl apply -f config-sync-operator.yaml
