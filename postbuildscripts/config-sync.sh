091d4f5bb43c83b53fafc798f587a982547dd5d9
Mohiddin Shaik <mshaik@lseg.com>
2021-07-08 15:03:03 +0100

    enable debug

--- postbuildscripts/config-sync.sh
+++ postbuildscripts/config-sync.sh
@@ -1,4 +1,4 @@
-set -ex
+set -xv  # Enable Debug
 
 gcloud container clusters get-credentials $1 --project $2 --zone $3
 gcloud compute ssh $4 --project $2 --zone $5 --tunnel-through-iap -- -L 3128:localhost:3128 -N -q -f
