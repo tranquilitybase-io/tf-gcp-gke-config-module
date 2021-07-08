03c34d9d13557ec016478e6ab6fc603047f44ceb
obrienalaribe <obrienalaribe@gmail.com>
2021-07-08 12:47:12 +0100

    Update config-sync.sh

--- postbuildscripts/config-sync.sh
+++ postbuildscripts/config-sync.sh
@@ -1,3 +1,5 @@
+echo "Switching user from root to cloudsdk ..."
+su - cloudsdk
 gcloud container clusters get-credentials $1 --project $2 --zone $3
 gcloud compute ssh $4 --project $2 --zone $5 --tunnel-through-iap -- -L 3128:localhost:3128 -N -q -f
 sleep 10
